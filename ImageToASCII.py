8463edca78ce9b8c8d0233a85400ac4e0d644407
Chloe Morgan <77995029+Chloe-Abby-Morgan@users.noreply.github.com>
2025-12-17 19:03:58 +0000

    Outputs to HTML
    Zooming out may be required depending on the image size

--- ImageToASCII.py
+++ ImageToASCII.py
@@ -1,12 +1,10 @@
-#Bug - This only works for square images (e.g width = 50 and height = 100 does not work)
-
 from PIL import Image
 import numpy
 
 image_file_path = "image.jpg"
-output_file_path = r"C:\Users\chloe\Desktop\Dev\Image to ASCII\Image-to-ASCII" + r"\output.txt"
+output_file_path = r"C:\Users\chloe\Desktop\Dev\Image to ASCII\Image-to-ASCII" + r"\output.html"
 image_width = 100
-image_height = 50
+image_height = 200
 char_list = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
 
 char_list = list(char_list)
@@ -15,17 +13,21 @@ char_list = list(char_list)
 img = Image.open(image_file_path).resize((image_width,image_height)).convert("L")
 img_array = numpy.array(img)
 
-output = numpy.empty(shape=(image_width,image_height), dtype=str)
+output = numpy.empty(shape=(image_height,image_width), dtype=str)
 
 #Loops through every element of the image array and converts it to its corresponding ASCII brightness value
-
 for row_index in range(len(img_array)):
     for pixel_index in range(len(img_array[row_index])):
         pixel_char = char_list[round(((len(char_list)-1)/256) * img_array[row_index, pixel_index])]
-        output[pixel_index, row_index] = pixel_char
+        output[row_index, pixel_index] = pixel_char
 
+#Writes to a HTML file
 with open(output_file_path, "w") as f:
+
+    print("<body style =\"background-color:black; color:white;\">",file=f)
+    print("<pre style>",file=f)
+
     for row in output:
-        print("".join(row), file=f)
+        print(" ".join(row), file=f)
 
 print("Image converted!" + "\n"  "Output saved to: " + output_file_path)
