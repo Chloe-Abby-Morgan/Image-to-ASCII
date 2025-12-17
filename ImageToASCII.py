4bc5039d01e7e84dedf00b84f080b3b0318d1f2a
Chloe Morgan <77995029+Chloe-Abby-Morgan@users.noreply.github.com>
2025-12-17 17:22:28 +0000

    Fixed Output Size Bug
    This no longer works with solely square images

--- ImageToASCII.py
+++ ImageToASCII.py
@@ -4,9 +4,9 @@ from PIL import Image
 import numpy
 
 image_file_path = "image.jpg"
-output_file_path = r"C:\Users\chloe\Desktop\Dev\Image to ASCII" + r"\output.txt"
+output_file_path = r"C:\Users\chloe\Desktop\Dev\Image to ASCII\Image-to-ASCII" + r"\output.txt"
 image_width = 100
-image_height = 100
+image_height = 50
 char_list = " `.-':_,^=;><+!rc*/z?sLTv)J7(|Fi{C}fI31tlu[neoZ5Yxjya]2ESwqkP6h9d4VpOGbUAKXHm8RD#$Bg0MNWQ%&@"
 
 char_list = list(char_list)
@@ -21,7 +21,7 @@ output = numpy.empty(shape=(image_width,image_height), dtype=str)
 
 for row_index in range(len(img_array)):
     for pixel_index in range(len(img_array[row_index])):
-        pixel_char = char_list[round(((len(char_list)-1)/256) * img_array[pixel_index, row_index])]
+        pixel_char = char_list[round(((len(char_list)-1)/256) * img_array[row_index, pixel_index])]
         output[pixel_index, row_index] = pixel_char
 
 with open(output_file_path, "w") as f:
